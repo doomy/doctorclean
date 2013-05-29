030a26404d9fda051dcd7471978dacaf644b499d
doomy <doomy@git>
2013-05-29 18:55:01 +0200

    Add "kontakt" content page;

---
+++ sql/upgrade/9.sql
@@ -0,0 +1,13 @@
+INSERT INTO t_content_pages (str_id, place, title, menu_title, content)
+    VALUES('kontakt', 6, 'Kontakt', 'Kontakt', '<h2>Provozovatel</h2>
+<p>
+    Marek Markov <br />
+    Praha 9, 190 00, Špitálská 2/700<br />
+    IČ: 87594242<br />
+    Bank. spojení: 670100-2202535619/6210
+</p>
+<h2>Obchodní zastoupení pro Prahu</h2>
+<p>
+    Email: <a href="mailto:doctorclean@seznam.cz">doctorclean@seznam.cz</a> <br />
+    Tel: +420 728 791 598
+</p>');
