5f09c334b785210972a43a919e9c181c26168cde
doomy <vladimir.bartek@gmail.com>
2013-07-08 17:05:26 +0200

    Remove obsolete file db.php

--- db.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $ENV_VARS['DB_HOST'] = 'localhost';
-    $ENV_VARS['DB_USER'] = 'doomy';
-    $ENV_VARS['DB_PASS'] = '0737735641';
-    $ENV_VARS['DB_NAME'] = 'doctorclean';
-    $ENV_VARS['DB_CREATE'] = true;
-?>
