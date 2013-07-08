15a3e4c584ad8d66846fe2e145fb08a90662ab5b
doomy <vladimir.bartek@gmail.com>
2013-07-08 17:20:40 +0200

    Show formatted content for content field in admin

--- templates/editor.tpl.php
+++ templates/editor.tpl.php
@@ -1,3 +1,12 @@
-<textarea>
-    <?php echo $content; ?>
-</textarea>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <script src="../../ckeditor/ckeditor.js"></script>
+    </head>
+    <body>
+        <textarea class="ckeditor" cols="50" id="editor1" name="editor1" rows="10" disabled >
+        <?php echo $content; ?>
+        </textarea>
+    </body>
+</html>
+
