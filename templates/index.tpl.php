b89186599dc7a043256d0838dd56d2602c82ae31
doomy <vladimir.bartek@gmail.com>
2013-07-10 09:43:44 +0200

    Show per-content page title in title bar

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="cs">
     <head>
-        <title>Doctor Clean</title>
+        <title><?php if($title!='DoctorClean') echo $title . " - "; ?>Doctor Clean</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	    <meta name="application-name" content="doctor-clean.cz" />
         <meta http-equiv="content-language" content="cs" />
