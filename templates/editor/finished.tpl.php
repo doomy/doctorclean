f6b0b658ad091a56ccdc95059cb138d5da6a3f75
doomy <vladimir.bartek@gmail.com>
2013-07-17 09:48:27 +0200

    Improve visuals in Finished page in Editor

--- templates/editor/finished.tpl.php
+++ templates/editor/finished.tpl.php
@@ -1,5 +1,16 @@
-<strong>Obsah uložen</strong>
-<br />
-<input type="button" value="Zpět" onclick="window.back();" />
-<input type="button" value="Zavřít" onclick="window.close();">
+<html>
+    <head>
+        <title>Hotovo</title>
+        <link rel="stylesheet" href="../../css/foundation.css" type="text/css">
+        <link rel="stylesheet" href="../../css/admin.css" type="text/css">
+        
+    </head>
+    <body>
+        <form class="content_saved">
+            <strong>Obsah uložen.</strong>
 
+            <input class="small button" type="button" value="Zpět" onclick="window.back();" />
+            <input class="small button" type="button" value="Zavřít" onclick="window.close();">
+        </form>
+    </body>
+</html>
