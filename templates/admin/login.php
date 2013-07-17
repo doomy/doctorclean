f2c653ad2cbc112b30ebb7ed9274f1fbbcbe6550
doomy <doomy@git>
2013-07-17 18:47:21 +0200

    Improve visuals of Admin Login

--- templates/admin/login.php
+++ templates/admin/login.php
@@ -3,18 +3,21 @@
     <head>
         <meta charset="utf-8">
         <title>Administrace</title>
+        <link rel="stylesheet" href="../css/foundation.css">
         <link rel="stylesheet" href="../css/admin.css">
     </head>
     <body>
-        <form id="login" method="POST">
-            <h1>Přihlášení</h1>
-            <fieldset id="inputs">
-                <input id="username" name="username" type="text" placeholder="Uživ. jméno" autofocus required>
-                <input id="password" name="password" type="password" placeholder="Heslo" required>
-            </fieldset>
-            <fieldset id="actions">
-                <input type="submit" id="submit" value="Přihlásit">
-            </fieldset>
-        </form>
+        <div class="login-content">
+            <form id="login" method="POST">
+                <h1>Přihlášení</h1>
+                <fieldset id="inputs">
+                    <input id="username" name="username" type="text" placeholder="Uživ. jméno" autofocus required>
+                    <input id="password" name="password" type="password" placeholder="Heslo" required>
+                </fieldset>
+                <fieldset id="actions">
+                    <input type="submit" class="button" id="submit" value="Přihlásit">
+                </fieldset>
+            </form>
+        </div>
     </body>
 </html>
