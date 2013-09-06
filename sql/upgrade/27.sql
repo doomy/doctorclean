84e93f4659f5666b3e0a018db7058653f3055b15
doomy <doomy@git>
2013-09-06 17:53:43 +0200

    Insert default values into content_image_file column in t_content_pages

---
+++ sql/upgrade/27.sql
@@ -0,0 +1,7 @@
+UPDATE t_content_pages SET content_image_file = 'uvod.png' WHERE str_id = 'uvod';
+UPDATE t_content_pages SET content_image_file = 'cisteni-kobercu.png' WHERE str_id = 'cisteni-kobercu';
+UPDATE t_content_pages SET content_image_file = 'cisteni-a-dezinfekce-klimatizace.png' WHERE str_id = 'cisteni-a-dezinfekce-klimatizace';
+UPDATE t_content_pages SET content_image_file = 'cisteni-aut.png' WHERE str_id = 'cisteni-aut';
+UPDATE t_content_pages SET content_image_file = 'myti-oken.png' WHERE str_id = 'myti-oken';
+UPDATE t_content_pages SET content_image_file = 'cenik.png' WHERE str_id = 'cenik';
+UPDATE t_content_pages SET content_image_file = 'kontakt.png' WHERE str_id = 'kontakt';
